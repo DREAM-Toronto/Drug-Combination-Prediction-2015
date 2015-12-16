6b39638f4b1d8f53293b2b284a26a8c48db4e4f1
ehsueh <ehsueh1993@gmail.com>
2015-12-15 20:27:27 -0500

    fixed format of output

--- allGenesInAllPathwaysContainingX.R
+++ allGenesInAllPathwaysContainingX.R
@@ -1,15 +1,16 @@
 
 allGenesInAllPathwaysContainingX = function(gene){
   raw <- read.csv("c6.all.v5.0.symbols.csv",
-                  head=FALSE,
-                  stringsAsFactors=FALSE)
+                  head = TRUE,
+                  stringsAsFactors = FALSE)
   containedGenes <- c()
   nRow <- nrow(raw)
   nCol <- ncol(raw)
   for(i in 1:nRow){
-    currRow <- raw[i,3:nCol]
+    currRow <- unlist(raw[i,3:nCol]) 
+    currRow <- currRow[currRow!=""] # ignore empty strings
     if(gene %in% currRow == TRUE){
-      containedGenes <- union(containedGenes, raw[i,3:nCol])
+      containedGenes <- union(containedGenes, currRow)
     }
   }
   return (containedGenes)
